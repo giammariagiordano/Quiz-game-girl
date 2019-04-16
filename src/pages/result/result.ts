2faac8ee731a65b0e2916cfbca4271dc73bd03e1
Francesco Vicidomini <fr.vicidomini94@gmail.com>
2019-04-16 18:23:00 +0200

    update score

--- src/pages/result/result.ts
+++ src/pages/result/result.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { HomePage } from '../home/home';
 import * as firebase from 'firebase';
-import { v } from '@angular/core/src/render3';
+//import { v } from '@angular/core/src/render3';
 
 /**
  * Generated class for the ResultPage page.
@@ -18,22 +17,17 @@ import { v } from '@angular/core/src/render3';
 })
 export class ResultPage {
   scoreTotal:number = 0;
-  private email:string;
-  private password:string;
-  private score:number;
-  private council:string;
-  private AnswerByUser:any;
+  private email: string;
+  private password: string;
+  private score: number;
+  private council: string;
+  private AnswerByUser: any;
+  private ref: any;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.score = navParams.get("score")
-   // this.AnswerByUser = navParams.get("ans");
-    //alert(JSON.stringify(this.AnswerByUser))
     this.email = localStorage.getItem("email")
-    this.password = localStorage.getItem("password")
-    firebase.auth().signInWithEmailAndPassword(this.email,this.password)
-      .then( res => firebase.database().ref('Users').orderByChild('email').equalTo(this.email).once('child_added', v => {
-      this.scoreTotal =  v.val().score+this.score;
-      }))
-      this.setCouncil(this.score);
+    //this.password = localStorage.getItem("password")
+    this.ref = firebase.database().ref("Users");
   }
 
   setCouncil(score:number){
@@ -49,5 +43,16 @@ export class ResultPage {
   }
   ionViewDidLoad() {
     console.log('ionViewDidLoad ResultPage');
+    this.ref.orderByChild('email').equalTo(this.email).on("child_added", (snapshot) => {
+      let result = snapshot.val();
+      let key = snapshot.key;
+      result.score += this.score;
+      this.scoreTotal = result.score;
+      this.ref.child(key).update(result);
+    });
+    
+    this.setCouncil(this.score);
   }
+
+
 }
